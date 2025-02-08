978aff1356bf2f27357a3cc1fd6e617c9a160275
Radio <radio@bemaniso.ws>
2025-02-08 12:24:07 +0100

    Improve logout loading state

--- app/main/logout.tsx
+++ app/main/logout.tsx
@@ -1,16 +1,18 @@
 import {Redirect} from "expo-router";
 import {useUserData} from "@/hooks/useUserData";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import FullScreenLoader from "@/components/FullScreenLoader";
 
 export default function logout() {
+    const [logoutLoading, setLogoutLoading] = useState(true);
     const {loading, clear} = useUserData();
 
     useEffect(() => {
         clear();
+        setLogoutLoading(false);
     }, []);
 
-    if(loading) {
+    if(loading || logoutLoading) {
         return <FullScreenLoader></FullScreenLoader>;
     }
 
