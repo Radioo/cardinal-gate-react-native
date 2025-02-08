99a063e483750af66addd4678ee6f78e3e2a02a5
Radio <radio@bemaniso.ws>
2025-02-08 17:01:25 +0100

    Add dark mode splash bg color

--- app/main/Home.tsx
+++ app/main/Home.tsx
@@ -1,15 +1,9 @@
-import {ThemedText} from "@/components/ThemedText";
-import {getSecureValue} from "@/store/secure";
-import {SecureValue} from "@/enums/secure-value";
 import {useUserData} from "@/hooks/useUserData";
 import FullScreenLoader from "@/components/FullScreenLoader";
 import {Animated, RefreshControl} from "react-native";
 import ScrollView = Animated.ScrollView;
-import {useEffect, useState} from "react";
-import fetchApi from "@/services/api";
+import {useState} from "react";
 import {PlayCounts} from "@/components/PlayCounts";
-import {Drawer} from "expo-router/drawer";
-import {Stack} from "expo-router";
 
 export default function Home() {
     const [refreshing] = useState(false);
