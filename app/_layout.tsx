0f2353a7c5611609edf24ef4f2395805808b28be
Radio <radio@bemaniso.ws>
2025-04-13 10:26:04 +0200

    Add app error boundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,32 @@
-import {Stack} from 'expo-router';
+import {ErrorBoundaryProps, Stack} from 'expo-router';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import {NotifierWrapper} from "react-native-notifier";
 import {ThemeProvider} from "@react-navigation/core";
 import {DarkTheme, DefaultTheme} from "@react-navigation/native";
 import {useColorScheme} from "@/hooks/useColorScheme";
 import {StatusBar} from "expo-status-bar";
-import {KeyboardAvoidingView, Platform} from "react-native";
+import {KeyboardAvoidingView, Platform, View} from "react-native";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {useReactQueryDevTools} from "@dev-plugins/react-query";
+import ThemedCard from "@/components/ThemedCard";
+import {ThemedText} from "@/components/ThemedText";
+import {ThemedButton} from "@/components/ThemedButton";
+import {useTheme} from "@/hooks/useTheme";
 
 const queryClient = new QueryClient();
+
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+    const theme = useTheme();
+
+    return (
+        <View style={{ flex: 1, backgroundColor: theme.background }}>
+            <ThemedCard style={{margin: 8, alignItems: 'center', justifyContent: 'center'}}>
+                <ThemedText>{error.message}</ThemedText>
+                <ThemedButton label="Retry" onPress={retry}/>
+            </ThemedCard>
+        </View>
+    );
+}
 
 export default function Layout() {
     useReactQueryDevTools(queryClient);
